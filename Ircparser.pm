790b86a186577c7e3816621e9dd8facb1685f87b
Trygve Aaberge <trygveaa@gmail.com>
2014-04-01 12:42:29 +0200

    Fix indentation in Ircparser.pm as well

--- Ircparser.pm
+++ Ircparser.pm
@@ -167,8 +167,8 @@ sub irc_private {
 }
 
 sub logger {
-	my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
-	for (split /\n/, shift) {
-		printf "%04d-%02d-%02d %02d:%02d:%02d - %s\n", $year+1900, $mon+1, $mday, $hour, $min, $sec, $_;
-	}
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+    for (split /\n/, shift) {
+        printf "%04d-%02d-%02d %02d:%02d:%02d - %s\n", $year+1900, $mon+1, $mday, $hour, $min, $sec, $_;
+    }
 }
